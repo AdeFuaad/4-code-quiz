0ddbb3409ea4fc7a4471432c3c66d949d0b31a26
AdeFuaad <adesholafuaad@gmail.com>
2023-03-25 14:46:12 -0400

    defined quiz questions

---
+++ script.js
@@ -0,0 +1,31 @@
+ // Define quiz questions and answers
+ const quiz = [
+    {
+      question: "What does HTML stand for?",
+      choices: ["Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyper Text Markup Language"],
+      answer: "Hyper Text Markup Language"
+    },
+    {
+      question: "What does CSS stand for?",
+      choices: ["Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets"],
+      answer: "Cascading Style Sheets"
+    },
+    {
+      question: "What does JS stand for?",
+      choices: ["JavaScript", "JavaSource", "JavaSauce"],
+      answer: "JavaScript"
+    },
+    {
+      question: "Which symbol is used for single-line comments in JavaScript?",
+      choices: ["//", "/*", "<!-- -->"],
+      answer: "//"
+    },
+    {
+      question: "What is the DOM in JavaScript?",
+      choices: ["Document Object Model", "Data Object Model", "Display Object Model"],
+      answer: "Document Object Model"
+    }
+  ];
+  
+  
+  

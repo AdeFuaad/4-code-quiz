31726a0ffd1193a7d7cd21823ee604b94d6b328c
AdeFuaad <adesholafuaad@gmail.com>
2023-03-26 00:27:17 -0400

    added the highscore save

--- script.js
+++ script.js
@@ -1,83 +1,287 @@
-// Define DOM elements
-const startButton = document.getElementById("start-button");
+const startButton = document.getElementById("start-btn");
 const quizContainer = document.getElementById("quiz-container");
 const questionElement = document.getElementById("question");
-const choicesElement = document.getElementById("choices");
-const timeLeftElement = document.getElementById("time-left");
-const gameOverContainer = document.getElementById("game-over-container");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const resultsContainer = document.getElementById("results-container");
+const initialsForm = document.getElementById("initials-form");
 const scoreElement = document.getElementById("score");
-const initialsElement = document.getElementById("initials");
-const submitScoreButton = document.getElementById("submit-score-button");
+const timerElement = document.getElementById("timer");
 
-// Define quiz questions and answers
-const quiz = [
+startButton.addEventListener("click", startQuiz);
+initialsForm.addEventListener("submit", saveScore);
+
+let currentQuestionIndex, timeLeft, timerInterval, score;
+
+const questions = [
   {
-    question: "What does HTML stand for?",
-    choices: [
-      "Hyperlinks and Text Markup Language",
-      "Home Tool Markup Language",
-      "Hyper Text Markup Language",
+    question: "What is the correct way to declare a variable in JavaScript?",
+    answers: [
+      { text: "int myVar;", correct: false },
+      { text: "let myVar;", correct: true },
+      { text: "variable myVar;", correct: false },
+      { text: "myVar = 0;", correct: false },
     ],
-    answer: "Hyper Text Markup Language",
   },
   {
-    question: "What does CSS stand for?",
-    choices: [
-      "Creative Style Sheets",
-      "Cascading Style Sheets",
-      "Computer Style Sheets",
+    question: "Which of the following is NOT a JavaScript data type?",
+    answers: [
+      { text: "String", correct: false },
+      { text: "Number", correct: false },
+      { text: "Boolean", correct: false },
+      { text: "Character", correct: true },
     ],
-    answer: "Cascading Style Sheets",
   },
   {
-    question: "What does JS stand for?",
-    choices: ["JavaScript", "JavaSource", "JavaSauce"],
-    answer: "JavaScript",
-  },
-  {
-    question: "Which symbol is used for single-line comments in JavaScript?",
-    choices: ["//", "/*", "<!-- -->"],
-    answer: "//",
-  },
-  {
-    question: "What is the DOM in JavaScript?",
-    choices: [
-      "Document Object Model",
-      "Data Object Model",
-      "Display Object Model",
+    question: "Which method is used to add an element to the end of an array?",
+    answers: [
+      { text: "push()", correct: true },
+      { text: "pop()", correct: false },
+      { text: "shift()", correct: false },
+      { text: "unshift()", correct: false },
     ],
-    answer: "Document Object Model",
+  },
+  {
+    question: "How do you write a comment in JavaScript?",
+    answers: [
+      { text: "// This is a comment", correct: true },
+      { text: "/* This is a comment", correct: false },
+      { text: "<!-- This is a comment -->", correct: false },
+      { text: "# This is a comment", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which of the following is the correct syntax for a function in JavaScript?",
+    answers: [
+      { text: "function myFunction() {}", correct: true },
+      { text: "def myFunction() {}", correct: false },
+      { text: "func myFunction() {}", correct: false },
+      { text: "function:myFunction() {}", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which of the following is NOT a valid way to create an object in JavaScript?",
+    answers: [
+      { text: "const obj = {};", correct: false },
+      { text: "const obj = new Object();", correct: false },
+      { text: "const obj = Object.create();", correct: false },
+      { text: "const obj = create Object();", correct: true },
+    ],
+  },
+  {
+    question: "Which method is used to remove the last element from an array?",
+    answers: [
+      { text: "push()", correct: false },
+      { text: "pop()", correct: true },
+      { text: "shift()", correct: false },
+      { text: "unshift()", correct: false },
+    ],
+  },
+  {
+    question: 'What is the output of the following code: "2" + 2?',
+    answers: [
+      { text: "4", correct: false },
+      { text: "22", correct: true },
+      { text: "NaN", correct: false },
+      { text: "undefined", correct: false },
+    ],
+  },
+  {
+    question: "Which of the following is NOT a falsy value in JavaScript?",
+    answers: [
+      { text: "0", correct: false },
+      { text: '""', correct: false },
+      { text: "null", correct: false },
+      { text: "1", correct: true },
+    ],
+  },
+  {
+    question:
+      "Which of the following is the correct syntax to create an instance of a Date object in JavaScript?",
+    answers: [
+      { text: "let today = Date();", correct: false },
+      { text: "let today = new Date;", correct: true },
+      { text: "let today = new date();", correct: false },
+      { text: "let today = Date.new;", correct: false },
+    ],
+  },
+  {
+    question: "What is the difference between let and var in JavaScript?",
+    answers: [
+      { text: "They are the same thing", correct: false },
+      { text: "let is block-scoped, var is function-scoped", correct: true },
+      { text: "var is block-scoped, let is function-scoped", correct: false },
+      { text: "let is hoisted, var is not", correct: false },
+    ],
+  },
+  {
+    question: "What is a closure in JavaScript?",
+    answers: [
+      { text: "A function that returns another function", correct: false },
+      { text: "A variable that cannot be changed", correct: false },
+      { text: "A data structure for storing key-value pairs", correct: false },
+      {
+        text: "A way to access an outer function's scope from an inner function",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "What is a higher-order function in JavaScript?",
+    answers: [
+      {
+        text: "A function that takes another function as an argument, or returns a function as a result",
+        correct: true,
+      },
+      {
+        text: "A function that has a higher priority than other functions",
+        correct: false,
+      },
+      { text: "A function that has more than one parameter", correct: false },
+      {
+        text: "A function that is used in functional programming",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "What is the difference between == and === in JavaScript?",
+    answers: [
+      { text: "They are the same thing", correct: false },
+      {
+        text: "== compares values, === compares values and types",
+        correct: true,
+      },
+      {
+        text: "== compares types, === compares values and types",
+        correct: false,
+      },
+      {
+        text: "== is used for assignment, === is used for comparison",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "What is the this keyword in JavaScript?",
+    answers: [
+      {
+        text: "A reserved keyword used for defining variables",
+        correct: false,
+      },
+      {
+        text: "A way to access an object's properties from within the object",
+        correct: false,
+      },
+      {
+        text: "A way to access a function's scope from within the function",
+        correct: false,
+      },
+      {
+        text: "A way to refer to the current object or context",
+        correct: true,
+      },
+    ],
+  },
+  {
+    question: "What is a callback function in JavaScript?",
+    answers: [
+      {
+        text: "A function that is called when an event occurs",
+        correct: false,
+      },
+      { text: "A function that is executed immediately", correct: false },
+      {
+        text: "A function that is passed as an argument to another function, and is called inside that function",
+        correct: true,
+      },
+      { text: "A function that is used to create new objects", correct: false },
+    ],
+  },
+  {
+    question:
+      "What is the difference between null and undefined in JavaScript?",
+    answers: [
+      { text: "They are the same thing", correct: false },
+      {
+        text: "null is a value that represents nothing, undefined means a variable has been declared but has not yet been assigned a value",
+        correct: true,
+      },
+      {
+        text: "null means a variable has been declared but has not yet been assigned a value, undefined is a value that represents nothing",
+        correct: false,
+      },
+      {
+        text: "null and undefined are both values that represent nothing",
+        correct: false,
+      },
+    ],
   },
 ];
 
-// Define global variables
-let currentQuestion = 0;
-let timeLeft = 75;
-let score = 0;
-
-// Event listener for start button
-startButton.addEventListener("click", startQuiz);
-
-// Function to start quiz
 function startQuiz() {
-  // Hide start button
-  startButton.style.display = "none";
-
-  // Show quiz container
-  quizContainer.style.display = "block";
-
-  // Start timer
-  const timer = setInterval(() => {
-    timeLeft--;
-    timeLeftElement.textContent = timeLeft;
-
-    if (timeLeft === 0) {
-      clearInterval(timer);
-      endQuiz();
-    }
-  }, 1000);
-
-  // Display first question
-  displayQuestion();
+  startButton.classList.add("hidden");
+  quizContainer.classList.remove("hidden");
+  score = 0;
+  currentQuestionIndex = 0;
+  timeLeft = 60;
+  timerInterval = setInterval(updateTime, 1000);
+  showQuestion();
 }
 
+function showQuestion() {
+  const question = questions[currentQuestionIndex];
+  questionElement.innerText = question.question;
+  answerButtonsElement.innerHTML = "";
+  question.answers.forEach((answer, index) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    button.addEventListener("click", () => selectAnswer(index, answer.correct));
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function selectAnswer(answerIndex, isCorrect) {
+  if (!isCorrect) {
+    timeLeft -= 10;
+  } else {
+    score++;
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    endQuiz();
+  }
+}
+
+function endQuiz() {
+  clearInterval(timerInterval);
+  quizContainer.classList.add("hidden");
+  resultsContainer.classList.remove("hidden");
+  scoreElement.textContent = score;
+}
+
+function updateTime() {
+  if (timeLeft <= 0) {
+    clearInterval(timerInterval);
+    endQuiz();
+  } else {
+    timerElement.innerText = `Time Left: ${timeLeft}s`;
+    timeLeft--;
+  }
+}
+
+function saveScore(e) {
+  e.preventDefault();
+  const initials = initialsForm.initials.value;
+  let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+  const newScore = { initials, score };
+  highscores.push(newScore);
+  highscores.sort((a, b) => b.score - a.score);
+  highscores = highscores.slice(0, 5); // Store only the top 5 high scores
+  localStorage.setItem("highscores", JSON.stringify(highscores));
+  window.location.href = "highscores.html"; // Redirect to a highscores page
+}
